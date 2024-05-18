795ebb38ee11d20f94eac314c588393927c2de92
Bruno Carvalho <carvalho.brunodc@gmail.com>
2024-05-18 14:47:42 -0300

    modificações das aulas de operadores

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String meuNome = "Bruno Carvalho";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        short numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(meuNome);
+        System.out.println(salarioMinimo);
+        System.out.println(numeroCurto2);
+        System.out.println(numero);
+
+    }
+}
